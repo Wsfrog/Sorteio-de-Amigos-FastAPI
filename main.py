d4f2e17e5b3265fa750ff9a2df77bc8bdfd59a1d
Wesley Oliveira <Wesleystarwars123@gmail.com>
2025-11-23 13:05:59 -0300

    "feat: enable CORS and enhance login response"

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, status, Depends
+from fastapi.middleware.cors import CORSMiddleware 
 from typing import List, Annotated
 from schemas import SorteioRequest, SorteioResponse, UsuarioCreate, UsuarioLogin, UsuarioPublic
 from logic import SorteadorService
@@ -11,6 +12,14 @@ app = FastAPI(
     summary="Sistema de Sorteio dos Pedradas"
 )
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],
+)
 
 def get_repo() -> UsuarioRepository:
     return db_instance
@@ -37,13 +46,19 @@ async def login(
     
     if not user_db or not SecurityService.verificar_senha(dados.senha, user_db['senha_hash']):
         raise HTTPException(status_code=401, detail="Credenciais inválidas")
-        
-    return {"mensagem": f"Bem-vindo de volta, {user_db['username']}! Login realizado."}
+    
+    return {
+        "mensagem": f"Bem-vindo de volta, {user_db['username']}! Login realizado.",
+        "usuario": {
+            "id": user_db["id"],
+            "username": user_db["username"],
+            "email": user_db["email"]
+        }
+    }
 
 @app.get("/usuarios", response_model=List[UsuarioPublic], tags=["Usuários"])
 async def listar_usuarios(repo: Annotated[UsuarioRepository, Depends(get_repo)]):
     return repo.listar_todos()
-
 
 @app.post("/sorteio", response_model=SorteioResponse, tags=["Sorteio"])
 async def criar_sorteio(
